972544a10f29f762561be623d6cd6f860a36dea7
Ukrit Pornpatanapaisarnkul <castlecs@CastleCSs-Mac-mini.local>
2016-10-11 14:20:44 -0700

    estimate create page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -78,8 +78,8 @@ Route::group(['middleware' => 'roles', 'roles' => ['Admin','Owner','Technician']
 
 // ESTIMATE ROUTES ------------------------------------
 Route::group(['middleware' => 'roles', 'roles' => ['Admin','Owner']], function () {
-    Route::get('estimate/create/{job}', ['as' => 'estimate.create', 'uses' => 'EstimateController@create']);
-    Route::resource('estimate', 'EstimateController', ['except' => ['create']]);
+    Route::get('estimates/create/{job}', ['as' => 'estimates.create', 'uses' => 'EstimateController@create']);
+    Route::resource('estimates', 'EstimateController', ['except' => ['create']]);
 });
 
 // PENDING INVOICE ROUTES - Only Admin & Owner access ------------------------------------
--- resources/views/estimates/create.blade.php
+++ resources/views/estimates/create.blade.php
@@ -10,15 +10,41 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8 col-md-offset-2">
         <h1>Create Estimate Invoice for Job ID: {{$job->id+20100}}</h1>
 
-        {{ Form::label('date_from', 'Issued Date:')}}
+        {!! Form::open(array('route' => 'estimates.store', 'data-parsley-validate'=>'')) !!}
+
+        {{ Form::label('date_from', 'Issued Date:', array('class'=>'control-label'))}}
         <div class="form-group input-group input-daterange">
             {{ Form::text('date_from',null, array('class' => 'form-control', 'id'=>'date_from','maxlength'=>'255'))}}
             <span class="input-group-addon">to</span>
             {{ Form::text('date_to',null, array('class' => 'form-control','id'=>'date_to', 'maxlength'=>'255'))}}
         </div>
+
+        <fieldset class="form-group required" >
+        {{ Form::label('description', 'Description:', array('class'=>'control-label'))  }}
+        {{ Form::textarea('description', null, array(
+            'class'    => 'form-control description',
+            'id'       => '',
+            'required' =>''
+        ))}}
+        </fieldset>
+
+        <fieldset class="form-group" >
+        {{ Form::label('extras', "Extra's:")  }}
+        {{ Form::textarea('extras', null, array(
+            'class'    => 'form-control description',
+            'id'       => 'extras',
+        ))}}
+        </fieldset>
+
+        {{ Form::submit('Create Estimate Invoice', array('class' => 'btn btn-success btn-lg btn-block btn-margin'))}}
+        <fieldset class="form-group">
+        {!! Html::linkRoute('jobs.show', 'Back to Job', array($job->id), array('class'=>'btn btn-danger  btn-lg btn-block btn-margin') ) !!}
+        </fieldset>
+
+        {!! Form::close() !!}
     </div>
 </div> <!-- /.row -->
 
