35fb703c5ac7b86c1592be095047d8bd63fae9af
Ukrit Pornpatanapaisarnkul <castlecs@CastleCSs-Mac-mini.local>
2016-10-12 12:12:45 -0700

    estimate invoice page

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -26,7 +26,7 @@
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Invoices <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="{{url('/invoices/pending/all')}}">All Approved Invoices</a></li>
+              <li><a href="">All Approved / Decline Invoices</a></li>
               <li role="separator" class="divider"></li>
               <li><a href="{{url('/invoices/pending/all')}}">All Pending Invoices</a></li>
               <li><a href="{{url('/invoices/pending/PC')}}">Pending Approval - Peter Campa</a></li>
